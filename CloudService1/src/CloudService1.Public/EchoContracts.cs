0e3613d90c9e67719eb13df2c21cdf92ea74f500
Sami Lamti <sami.lamti@if.se>
2016-06-08 10:07:57 +0200

    Contracts for Cloud Service 1

---
+++ CloudService1/src/CloudService1.Public/EchoContracts.cs
@@ -0,0 +1,17 @@
+﻿namespace CloudService1.Public.Commands
+{
+    public interface PleaseRepeatThis
+    {
+        string Phrase { get; set; }
+    }
+}
+
+namespace CloudService1.Public.Events
+{
+    public interface EchoedResponse
+    {
+        string EchoedPhrase { get; set; }
+    }
+}
+
+
